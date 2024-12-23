2be985d62014fe1da93ff4b54d07ff15ca33278d
Ehsan Norouzi <ehsannorouzi@protonmail.com>
2024-12-23 15:05:30 +0330

    feat: background and new page

--- app/src/main/java/ir/shams/login/MainActivity.java
+++ app/src/main/java/ir/shams/login/MainActivity.java
@@ -1,20 +1,15 @@
 package ir.shams.login;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-
 
 public class MainActivity extends AppCompatActivity {
     String USERNAME = "Ehsan";
     String PASSWORD = "1234";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
